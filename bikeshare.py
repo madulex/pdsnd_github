447ffc8feb26bf9ba9bfca4761774546a4321c9f
Anyimadu <amadulex@gmail.com>
2019-12-17 09:51:21 +0000

    first trial of bikeshare commit

--- bikeshare.py
+++ bikeshare.py
@@ -2,7 +2,7 @@ import time
 import pandas as pd
 import numpy as np
 import collections as cl
-'''The upload of .csv files from source into the editor''' 
+'''The upload of .csv files from source into the editor'''
 CITY_DATA = { 'chicago': 'chicago.csv',
               'new york city': 'new_york_city.csv',
               'washington': 'washington.csv' }
